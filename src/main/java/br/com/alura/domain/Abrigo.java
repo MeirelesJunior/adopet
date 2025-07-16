47bd5feb63eab88ad0f28f991e6e89bb6c13e161
MeirelesJunior <junior_turren@hotmail.com>
2025-07-16 10:10:45 -0300

    refactor(service): substitui manipulação manual de JSON por Jackson nas classes de serviço

--- src/main/java/br/com/alura/domain/Abrigo.java
+++ src/main/java/br/com/alura/domain/Abrigo.java
@@ -4,6 +4,10 @@ public class Abrigo {
     private String nome;
     private String telefone;
     private String email;
+    private Long id;
+    private Pet[] pets;
+
+    public Abrigo(){}
 
     public Abrigo(String nome, String telefone, String email) {
         this.nome = nome;
@@ -11,5 +15,23 @@ public class Abrigo {
         this.email = email;
     }
 
+    public String getNome() {
+        return nome;
+    }
 
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Pet[] getPets() {
+        return pets;
+    }
 }
--- src/main/java/br/com/alura/domain/Pet.java
+++ src/main/java/br/com/alura/domain/Pet.java
@@ -2,12 +2,15 @@ package br.com.alura.domain;
 
 public class Pet {
 
+    private Long id;
     private String tipo;
     private String nome;
     private String raca;
     private Integer idade;
     private String cor;
     private Double peso;
+
+    public Pet(){}
 
     public Pet(String tipo, String nome, String raca, Integer idade, String cor, Double peso) {
         this.tipo = tipo;
@@ -16,5 +19,34 @@ public class Pet {
         this.idade = idade;
         this.cor = cor;
         this.peso = peso;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public String getCor() {
+        return cor;
     }
 }
